797fc7ed163311dbf924923012946b6cd8434237
G-gonn <skander.apollo@gmail.com>
2017-12-14 19:08:27 +0100

    Devise & Views commit

---
+++ app/assets/javascripts/new.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+    $("ul.tabs").each(function() {
+
+        var $stylecss, $section, $links = $(this).find('a');
+
+        //'find' permet d'obtenir les descendants de chaque élément.
+
+        //$(this) sert à retourner un objet jQuery à partir de l'objet Javascript natif this
+
+        $stylecss = $($links.filter("[href='" + location.hash + "']")[0] || $links[0]);
+
+        console.log($stylecss);
+
+        $stylecss.addClass('stylecss');
+
+        $section = $($stylecss[0].hash);
+
+        $links.not($stylecss).each(function() {
+
+            $(this.hash).hide();
+
+        });
+
+        $(this).on('click', 'a', function(event) {
+
+            $stylecss.removeClass('stylecss');
+            $section.hide();
+
+            $stylecss = $(this);
+            $section = $(this.hash);
+
+            $stylecss.addClass('stylecss');
+            $section.show();
+
+            event.preventDefault();
+
+            //Empecher l'action par défaut de l'événement de se déclencher.
+
+        });
+
+    });
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   
   root 'home#index'
 
-  get 'home/private'
+  get '/home', to: 'home#private'
+
+  get '/signup', to: 'devise/sessions#new'
+
+  get '/profile', to: 'home#profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
